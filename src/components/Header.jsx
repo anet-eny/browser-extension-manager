fd8cc5c44b94db3fcf95a2b6e5f5ee35dace2155
anet-eny <anet.eny@gmail.com>
2025-10-11 16:59:51 +0200

    Implement dark mode to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,16 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-neutral-0 flex items-center px-3 py-2 mt-5 mx-4 rounded-xl shadow-md border border-[oklch(0.9095_0.029_259.59)]">
+    <header className="bg-neutral-0 dark:bg-neutral-800 flex items-center px-3 py-2 mt-5 mx-4 rounded-xl shadow-md border border-[oklch(0.9095_0.029_259.59)]">
       <div>
-        <img src={logoLight} alt="Logo" />
+        <img src={theme === "light" ? logoLight : logoDark} alt="Logo" />
       </div>
-      <button className="bg-neutral-100 w-[50px] h-[50px] flex justify-center items-center rounded-xl ml-auto">
-        <img src={iconMoon} alt="Dark mode" />
+      <button
+        onClick={toggleTheme}
+        className="bg-neutral-100 dark:bg-neutral-700 w-[50px] h-[50px] flex justify-center items-center rounded-xl ml-auto"
+        aria-label="Toggle theme"
+      >
+        <img src={theme === "light" ? iconMoon : iconSun} alt="Dark mode" />
       </button>
     </header>
   );
