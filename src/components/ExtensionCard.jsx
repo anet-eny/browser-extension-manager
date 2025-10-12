337526ae851279ba205c5039d60f704710942528
anet-eny <anet.eny@gmail.com>
2025-10-12 11:54:47 +0200

    Create ExtensionCards section in Home, add IDs to extensions data

---
+++ src/components/ExtensionCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ExtensionCard() {
+  return (
+    <article>
+      <div></div>
+    </article>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { ExtensionsContext } from "../context/ExtensionsContext";
 import Header from "../components/Header";
 import FilterButton from "../components/FilterButton";
+import ExtensionCard from "../components/ExtensionCard";
 
 export default function Home() {
+  const { getFilteredItems, toggleActive } = useContext(ExtensionsContext);
+  const items = getFilteredItems();
+
   return (
     <div className="min-h-screen pt-5 px-4 bg-gradient-to-b from-gradientlight-start to-gradientlight-end dark:from-gradientdark-start dark:to-gradientdark-end">
       <Header />
@@ -13,6 +19,20 @@ export default function Home() {
           <FilterButton value="active">Active</FilterButton>
           <FilterButton value="inactive">Inactive</FilterButton>
         </nav>
+
+        <section>
+          {items.length === 0 ? (
+            <div>No extensions found</div>
+          ) : (
+            items.map((item) => (
+              <ExtensionCard
+                key={item.id}
+                item={item}
+                onToggle={toggleActive}
+              />
+            ))
+          )}
+        </section>
       </main>
     </div>
   );
