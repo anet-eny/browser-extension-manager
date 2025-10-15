b443b10822ac9e8403c5c9c424e655269edd4d49
anet-eny <anet.eny@gmail.com>
2025-10-15 13:22:39 +0200

    Update Home layout styles and fix body background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const items = getFilteredItems();
 
   return (
-    <div className="min-h-screen pt-5 px-4 bg-gradient-to-b from-gradientlight-start to-gradientlight-end dark:from-gradientdark-start dark:to-gradientdark-end">
+    <div className="max-w-5xl min-h-screen mx-auto pt-5 sm:pt-6 lg:pt-10 px-4 sm:px-8">
       <Header />
 
       <main className="flex flex-col justify-center items-center my-10 gap-6 dark:text-neutral-0">
@@ -20,7 +20,7 @@ export default function Home() {
           <FilterButton value="inactive">Inactive</FilterButton>
         </nav>
 
-        <section>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
           {items.length === 0 ? (
             <div>No extensions found</div>
           ) : (
