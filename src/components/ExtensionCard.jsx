90c225c2f5559151a92f71777c07fd68148bdf49
anet-eny <anet.eny@gmail.com>
2025-10-15 12:32:20 +0200

    Update styles of ExtensionCard buttons

--- src/components/ExtensionCard.jsx
+++ src/components/ExtensionCard.jsx
@@ -3,7 +3,7 @@ import ToggleSwitch from "./ToggleSwitch";
 
 export default function ExtensionCard({ item, onToggle }) {
   return (
-    <article className="p-5 bg-neutral-0">
+    <article className="p-5 bg-neutral-0 rounded-[20px] min-h-[200px]">
       <div className="flex gap-4">
         <img src={item.logo} alt={`${item.name} icon`} />
         <div>
@@ -11,8 +11,10 @@ export default function ExtensionCard({ item, onToggle }) {
           <p className="text-preset-5 text-neutral-600">{item.description}</p>
         </div>
       </div>
-      <div>
-        <button>Remove</button>
+      <div className="flex justify-between items-center mt-6">
+        <button className="px-4 py-2 text-preset-6 text-neutral-900 border border-neutral-300 rounded-full cursor-pointer">
+          Remove
+        </button>
 
         <ToggleSwitch
           id={item.id}
--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -10,9 +10,9 @@ export default function ToggleSwitch({ id, checked, onChange }) {
         aria-checked={checked}
       />
 
-      <div className="w-9 h-5 rounded-full bg-gray-300 transition-colors peer-checked:bg-red-700"></div>
+      <div className="w-9 h-5 rounded-full bg-gray-300 transition-colors peer-checked:bg-red-700 cursor-pointer"></div>
 
-      <div className="absolute top-0.5 left-0.5 w-4 h-4 rounded-full bg-white transition-transform peer-checked:translate-x-4" />
+      <div className="absolute top-0.5 left-0.5 w-4 h-4 rounded-full bg-white transition-transform peer-checked:translate-x-4 cursor-pointer" />
     </label>
   );
 }
